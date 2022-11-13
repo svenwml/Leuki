a011be83db2b62b7f59e0d7c55f13596ab527bca
Sven W <svenwml@yahoo.de>
2022-11-13 09:42:31 +0100

    rename custom plot widget, add basic interactions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectAxes |
+                                      QCP::iSelectLegend | QCP::iSelectPlottables);
 }
 
 MainWindow::~MainWindow()
